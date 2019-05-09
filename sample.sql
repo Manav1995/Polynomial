4001bafa50d322705de609e6730155ebecb9345d
Manav Veer gulati <manav.veergulati19@gmail.com>
2019-05-09 15:25:25 +0200

    Update sample.sql

--- sample.sql
+++ sample.sql
@@ -1,3 +1,4 @@
 
- insert into stg_external_apis.dummy (xxxx) values ('hello');
+ insert into stg_external_apis.dummy (xxxx) values ('hello'); insert into stg_external_apis.dummy (xxxx) values ('hello2');
+
 
